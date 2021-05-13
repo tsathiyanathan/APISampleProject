dc32cc8ea19d332be9d8785aed9678d880c03df5
tsathiyanathan <83004686+tsathiyanathan@users.noreply.github.com>
2021-05-13 20:55:49 +0530

    Added the Correct URL

--- src/LoginPage/LoginRestAPI.java
+++ src/LoginPage/LoginRestAPI.java
@@ -4,39 +4,46 @@ import org.json.simple.JSONObject;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import io.restassured.RestAssured;
-import io.restassured.http.Method;
+
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
+
+@SuppressWarnings("deprecation")
 public class LoginRestAPI {
 
+@SuppressWarnings("unchecked")
 @Test
   public void f() {
 	  
-	  RestAssured.baseURI ="https://cc.healthrecoverysolutions.com/login";
+	  RestAssured.baseURI ="https://gateway.healthrecoverysolutions.com";
 
 	  RequestSpecification request = RestAssured.given();
 	   
-		request.header("Content-Type", "application/json");
+	   request.header("Content-Type", "application/json");
+	   
 		
 		JSONObject requestParams = new JSONObject();
 		requestParams.put("type","credentials");
 		requestParams.put("username","test");
 		requestParams.put("password","test");
 		requestParams.put("source","ClinicianConnect 2");
-
-	  
-       request.body(requestParams.toJSONString());
+		
+		JSONObject dataparams = new JSONObject();
+		dataparams.put("data", requestParams);
+		
+	    request.body(dataparams.toJSONString());
 
 	  // Post the request and check the response
 
-	  Response response = request.post("https://gateway.healthrecoverysolutions.com/v1/tokens/");
+	  Response response = request.post("/v1/tokens");
 
 	  String responsebody=response.asString();
 	
       System.out.println("Response Body is :"+ responsebody);
 	  
 	  int statusCode = response.getStatusCode();
+	  
 	  Assert.assertEquals(statusCode, 422);
 	 
 	  
@@ -47,6 +54,8 @@ public class LoginRestAPI {
 	  String serverType =  response.header("Server");
 	  
 	  Assert.assertEquals(serverType, "nginx");
+	
+	
 	    
 	
 	  
